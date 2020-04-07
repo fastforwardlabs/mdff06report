9816662463b38a00f3434cdbf6e1be42231f014c
grant <grantcuster@gmail.com>
2020-04-07 16:46:40 -0400

    update google analytics

--- build_html.js
+++ build_html.js
@@ -616,7 +616,7 @@ function wrap(content) {
         m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
         })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-        ga('create', 'UA-157475426-2', 'auto');
+        ga('create', 'UA-157475426-4', 'auto');
         ga('send', 'pageview');
 
         window.addEventListener('load', function() {
