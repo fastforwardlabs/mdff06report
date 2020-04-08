d153c0cdde77cb2eb13ba8ab9ee9a5ea6c61e719
grant <grantcuster@gmail.com>
2020-04-08 12:05:08 -0400

    try google analytics again

--- build_html.js
+++ build_html.js
@@ -581,15 +581,6 @@ return `<head>
 ${makeStyle()}
 ${makeJS()}
 
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-140718127-1"></script>
-<script>
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-
-gtag('config', 'UA-140718127-1');
-</script>
 </head>`
 }
 
